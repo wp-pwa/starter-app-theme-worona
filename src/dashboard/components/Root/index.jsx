076a1a3124a6e6033559cd79e12466fa7430a4c2
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-12-28 16:42:37 +0100

    Added ColorPicker

--- src/dashboard/components/Root/index.jsx
+++ src/dashboard/components/Root/index.jsx
@@ -1,52 +1,96 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field, change } from 'redux-form';
 import { connect } from 'react-redux';
+import { ChromePicker } from 'react-color';
 import '!style!css!postcss!react-ios-switch/build/bundle.css';
 import Switch from './Switch';
 import * as deps from '../../deps';
 
-let StarterThemeForm = ({ pristine, waiting }) => {
-  const Button = deps.elements.Button;
-  const Icon = deps.elements.Icon;
-  return (
-    <form>
-      <p className="control">
-        <label className="label" htmlFor="color">Color</label>
-      </p>
-      <p className="control">
-        <Button size="medium">
-          <Icon small code="paint-brush" />
-          <span>Change color</span>
-        </Button>
-      </p>
-      <Field
-        name="displayFeaturedImage"
-        component={Switch}
-        label="Display featured image?"
-        type="checkbox"
-      />
-      <br />
-      <Field
-        name="displayCategories"
-        component={Switch}
-        label="Display categories menu?"
-        type="checkbox"
-      />
-      <br />
-      <Button
-        color="primary"
-        size="large"
-        type="submit"
-        disabled={pristine}
-        loading={waiting}
-      >
-        Save
-      </Button>
-    </form>
-  );
-};
+class StarterThemeFormClass extends React.Component {
 
-StarterThemeForm.propTypes = {
+  constructor(props) {
+    super(props);
+    this.state = { showColorPicker: false };
+    this.toggleColorPicker = this.toggleColorPicker.bind(this);
+    this.hideColorPicker = this.hideColorPicker.bind(this);
+  }
+
+  toggleColorPicker() {
+    this.setState({ showColorPicker: !this.state.showColorPicker });
+  }
+
+  hideColorPicker() {
+    this.setState({ showColorPicker: false });
+  }
+
+  render() {
+    const { pristine, waiting, updateColorSelected, chosenColor } = this.props;
+    const Button = deps.elements.Button;
+    const Icon = deps.elements.Icon;
+    const cover = {
+      position: 'fixed',
+      top: '0px',
+      right: '0px',
+      bottom: '0px',
+      left: '0px',
+    };
+    const popover = {
+      position: 'absolute',
+      zIndex: '2',
+    };
+    return (
+      <form>
+        <p className="control">
+          <label className="label" htmlFor="color">Color</label>
+        </p>
+        <p className="control">
+          <Button size="medium" onClick={this.toggleColorPicker}>
+            <Icon small code="paint-brush" />
+            <span>Change color</span>
+          </Button>
+        </p>
+        { this.state.showColorPicker ?
+          <div style={popover}>
+            <div style={cover} onClick={this.hideColorPicker} />
+            <ChromePicker onChangeComplete={updateColorSelected} color={chosenColor} />
+          </div>
+        : null }
+        <Field
+          name="chosenColor"
+          component="input"
+          type="hidden"
+        />
+        <Field
+          name="displayFeaturedImage"
+          component={Switch}
+          label="Display featured image?"
+          type="checkbox"
+        />
+        <br />
+        <Field
+          name="displayCategories"
+          component={Switch}
+          label="Display categories menu?"
+          type="checkbox"
+        />
+        <br />
+        <Button
+          color="primary"
+          size="large"
+          type="submit"
+          disabled={pristine}
+          loading={waiting}
+        >
+          Save
+        </Button>
+      </form>
+    );
+  }
+}
+
+StarterThemeFormClass.propTypes = {
+  chosenColor: React.PropTypes.string,
+  updateColorSelected: React.PropTypes.func.isRequired,
   handleSubmit: React.PropTypes.func.isRequired,
   waiting: React.PropTypes.bool,
   siteId: React.PropTypes.string,
@@ -55,14 +99,22 @@ StarterThemeForm.propTypes = {
 
 const mapStateToFormProps = state => ({
   waiting: state.settings.savingSettings === 'starter-app-theme-worona',
+  chosenColor: state.theme.reduxForm.StarterThemeForm
+    && state.theme.reduxForm.StarterThemeForm.values
+    && state.theme.reduxForm.StarterThemeForm.values.chosenColor
+    && state.theme.reduxForm.StarterThemeForm.values.chosenColor.hex,
 });
 
-StarterThemeForm = reduxForm({
+const mapDispatchToFormProps = dispatch => ({
+  updateColorSelected: (color) => dispatch(change('StarterThemeForm', 'chosenColor', color)),
+});
+
+let StarterThemeForm = reduxForm({
   form: 'StarterThemeForm',
-  fields: ['displayCategories', 'displayFeaturedImage'],
+  fields: ['chosenColor', 'displayCategories', 'displayFeaturedImage'],
   getFormState: state => state.theme.reduxForm,
-})(StarterThemeForm);
-StarterThemeForm = connect(mapStateToFormProps)(StarterThemeForm);
+})(StarterThemeFormClass);
+StarterThemeForm = connect(mapStateToFormProps, mapDispatchToFormProps)(StarterThemeForm);
 
 export default () => {
   const RootContainer = deps.elements.RootContainer;
