4e53c63cea3df6abc503ded470e16f6cbbbd6d61
Luis Herranz <luisherranz@gmail.com>
2017-01-17 11:11:19 +0100

    Show list of posts.

--- src/app/components/Theme/index.jsx
+++ src/app/components/Theme/index.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as deps from '../../deps';
 
-const Theme = ({ theme, generalApp }) => (
+const Theme = ({ theme, generalApp, postsResult, posts, isReady }) => (
   <div>
     <div>title: {generalApp.title}</div>
     <div>numberOfPosts: {generalApp.numberOfPosts}</div>
     <div>chosenColor: {theme.chosenColor}</div>
     <div>displayFeaturedImage: {theme.displayFeaturedImage ? 'yes' : 'no'}</div>
     <div>displayCategories: {theme.displayCategories ? 'yes' : 'no'}</div>
+    <ul>
+      {isReady && postsResult.map(id =>
+        <li>{posts[id].title.rendered}</li>
+      )}
+      {!isReady && (<div>Loding...</div>)}
+    </ul>
   </div>
 );
 
@@ -27,6 +33,9 @@ Theme.propTypes = {
 const mapStateToProps = (state) => ({
   generalApp: deps.selectorCreators.getSettings('generalApp')(state),
   theme: deps.selectorCreators.getSettings('theme')(state),
+  isReady: state.connection && state.connection.posts && state.connection.posts.isReady,
+  posts: state.connection && state.connection.posts && state.connection.posts.entities.post,
+  postsResult: state.connection && state.connection.posts && state.connection.posts.result,
 });
 
 export default connect(mapStateToProps)(Theme);
