14ef644c47e6d875d2a6c83533194bd4b0c6804c
Luis Herranz <luisherranz@gmail.com>
2017-01-27 13:50:45 +0100

    Remove sagas in app.

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable global-require, import/no-dynamic-require */
 import './style.sass';
-import * as sagas from './sagas';
 import * as reducers from './reducers';
 import * as components from './components';
 import * as deps from './deps';
@@ -8,7 +7,6 @@ import * as deps from './deps';
 const locales = lang => require(`./locales/${lang}.json`);
 
 export {
-  sagas,
   reducers,
   components,
   deps,
