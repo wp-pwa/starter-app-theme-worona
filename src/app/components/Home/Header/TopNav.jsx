6939ce505d9fe1dd1cec129a572ef2d1b4437d30
Luis Herranz <luisherranz@gmail.com>
2017-02-07 11:27:23 +0100

    Example of iOS only css.

--- src/app/components/Home/Header/TopNav.jsx
+++ src/app/components/Home/Header/TopNav.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { isIos } from 'worona-deps';
 import { connect } from 'react-redux';
+import cn from 'classnames';
 import * as deps from '../../../deps';
 import * as libs from '../../../libs';
 import styles from './style.css';
 
-const TopNav = ({ title, chosenColor }) => (
+const TopNav = ({ title, chosenColor, ios }) => (
   <div
-    className={styles.headMenu}
+    className={cn(styles.headMenu, ios && styles.headMenuIos)}
     style={{ backgroundColor: chosenColor, color: libs.blackOrWhite(chosenColor) }}
   >
     {title}
   </div>
 );
 
-TopNav.propTypes = { title: React.PropTypes.string, chosenColor: React.PropTypes.string };
+TopNav.propTypes = {
+  title: React.PropTypes.string,
+  chosenColor: React.PropTypes.string,
+  ios: React.PropTypes.bool,
+};
 
 const mapStateToProps = state => ({
   // eslint-disable-line
   title: deps.selectorCreators.getSetting('generalApp', 'title')(state),
   chosenColor: deps.selectorCreators.getSetting('theme', 'chosenColor')(state),
+  ios: isIos,
 });
 
 export default connect(mapStateToProps)(TopNav);
