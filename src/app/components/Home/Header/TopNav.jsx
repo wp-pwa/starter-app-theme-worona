7a66a1b0a5f04fbd2da3478e54430c9d1cf100f3
Luis Herranz <luisherranz@gmail.com>
2017-04-03 13:44:05 +0200

    Remove extra padding in iOS

--- src/app/components/Home/Header/TopNav.jsx
+++ src/app/components/Home/Header/TopNav.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { isIos } from 'worona-deps';
 import { connect } from 'react-redux';
-import cn from 'classnames';
 import * as deps from '../../../deps';
 import * as libs from '../../../libs';
 import styles from './style.css';
 
-const TopNav = ({ title, chosenColor, ios }) => (
+const TopNav = ({ title, chosenColor }) => (
   <div
-    className={cn(styles.headMenu, ios && styles.headMenuIos)}
+    className={styles.headMenu}
     style={{ backgroundColor: chosenColor, color: libs.blackOrWhite(chosenColor) }}
   >
     {title}
@@ -18,14 +16,12 @@ const TopNav = ({ title, chosenColor, ios }) => (
 TopNav.propTypes = {
   title: React.PropTypes.string,
   chosenColor: React.PropTypes.string,
-  ios: React.PropTypes.bool,
 };
 
 const mapStateToProps = state => ({
   // eslint-disable-line
   title: deps.selectorCreators.getSetting('generalApp', 'title')(state),
   chosenColor: deps.selectorCreators.getSetting('theme', 'chosenColor')(state),
-  ios: isIos,
 });
 
 export default connect(mapStateToProps)(TopNav);
