723fda508564405d14dcc7b8c2480b88805561a7
Luis Herranz <luisherranz@gmail.com>
2017-04-03 13:56:00 +0200

    Add forgotten import

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+import { takeEvery } from 'redux-saga';
 import { select, fork } from 'redux-saga/effects';
 import { isIos } from 'worona-deps';
 import { blackOrWhite } from '../libs';
@@ -7,27 +8,22 @@ import * as deps from '../deps';
 function* colorStatusBar() {
   const contentType = yield select(deps.selectors.getContentType);
   const color = yield select(deps.selectorCreators.getSetting('theme', 'chosenColor'));
-  console.log(contentType);
   if (contentType === 'Post' || contentType === 'Page') {
     // White background and dark text.
     StatusBar.backgroundColorByHexString('#FFF');
     StatusBar.styleDefault();
-    console.log('to white');
   } else {
     // background of same color than app and text depending on blackOrWhite color.
     StatusBar.backgroundColorByHexString(color);
     if (blackOrWhite(color) === '#FFF') StatusBar.styleLightContent();
     else StatusBar.styleDefault();
-    console.log('to color');
   }
 }
 
 export default function* starterProSagas() {
   if (isIos && StatusBar) {
     StatusBar.overlaysWebView(false);
-    yield [
-      fork(colorStatusBar),
-      takeEvery(deps.types.ROUTER_DID_CHANGE, colorStatusBar),
-    ];
+    yield fork(colorStatusBar);
+    yield takeEvery(deps.types.ROUTER_DID_CHANGE, colorStatusBar);
   }
 }
